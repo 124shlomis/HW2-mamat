c4f8b5ab60f84d43ac42afa9f310c1cbe4888244
Shlomi Shitrit <124shlomis@gmail.com>
2020-05-05 21:22:18 +0300

    First Upload
    initilization

---
+++ cmake-build-debug/CMakeFiles/HW2.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/HW2.dir/main.c.o"
+  "CMakeFiles/HW2.dir/party.c.o"
+  "CMakeFiles/HW2.dir/voter.c.o"
+  "HW2"
+  "HW2.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/HW2.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
